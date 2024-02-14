88b6e6174d681f59ab4d5019c1405ec0d71134dc
satyam kale <satyamkale271@gmail.com>
2024-02-15 00:18:00 +0530

    added logic of mark done tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -14,19 +15,19 @@ export default function App() {
   );
 }
 function Logo() {
-  return <h1>🌴Far away 👜</h1>;
+  return <h1>🌴 Far away 👜</h1>;
 }
 function Form() {
   return (
     <div className="add-form">
-      <h2>what do you need for your trip?</h2>
+      <h3>what do you need for your trip?</h3>
     </div>
   );
 }
 function PackingList() {
   return (
-    <div>
-      <ul className="list">
+    <div className="list">
+      <ul>
         {initialItems.map((item) => (
           <Item item={item} />
         ))}
@@ -36,7 +37,14 @@ function PackingList() {
 }
 
 function Item({ item }) {
-  return <li>{item.description}</li>;
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
