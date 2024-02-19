0d6a1ff48cdbaecf24a600acfe6ad68089c64454
satyam kale <satyamkale271@gmail.com>
2024-02-19 21:35:27 +0530

    noitems display message added

--- src/App.js
+++ src/App.js
@@ -111,6 +111,13 @@ function Item({ item, onDeleteItem, onToggleItems }) {
 }
 
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀 </em>
+      </p>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
