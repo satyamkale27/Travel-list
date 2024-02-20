5f53b56b58ae41be6a1eb5a902dc42a74c9c0c3e
satyam kale <satyamkale271@gmail.com>
2024-02-20 22:42:10 +0530

    filter structure add

--- src/App.js
+++ src/App.js
@@ -78,6 +78,8 @@ function Form({ onAddItems }) {
 }
 function PackingList({ items, onDeleteItem, onToggleItems }) {
   // onDeleteItem added new prop //
+
+  const [SortBy, setSortBy] = useState("input");
   return (
     <div className="list">
       <ul>
@@ -90,6 +92,13 @@ function PackingList({ items, onDeleteItem, onToggleItems }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={SortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
