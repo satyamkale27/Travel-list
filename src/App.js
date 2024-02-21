e98535d17c6927d12e1909079f8795d3bff0428d
satyam kale <satyamkale271@gmail.com>
2024-02-21 18:05:07 +0530

    stats added in seperate files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
-import Item from "./Item";
+import Stats from "./Stats";
+
 export default function App() {
   const [items, setitems] = useState([]); // moved the state to parent component of packing list //
 
@@ -35,29 +36,5 @@ export default function App() {
       />
       <Stats items={items} />
     </div>
-  );
-}
-
-function Stats({ items }) {
-  if (!items.length)
-    return (
-      <p className="stats">
-        <em>Start adding some items to your packing list 🚀 </em>
-      </p>
-    );
-
-  const numItems = items.length;
-  const numPacked = items.filter((item) => item.packed).length;
-  const percentage = Math.round((numPacked / numItems) * 100);
-  return (
-    <footer className="stats">
-      <em>
-        {percentage === 100
-          ? "You got everything! Ready to go ✈"
-          : `you have ${numItems} items on your list, you already packed
-         ${numPacked}
-        (${percentage}%)`}
-      </em>
-    </footer>
   );
 }
