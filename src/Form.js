c0e4746941bf5757d087ba6e895b9d21c725ac07
satyam kale <satyamkale271@gmail.com>
2024-02-21 16:59:37 +0530

    made Form.js and added code in Form.js seperate component

---
+++ src/Form.js
@@ -0,0 +1,38 @@
+function Form({ onAddItems }) {
+  const [description, setdescription] = useState("");
+  const [Quantity, setQuantity] = useState(1);
+  function handelsubmit(e) {
+    e.preventDefault(); // e is event handler and preventdefault()  function prevents reloading of page
+    // (Number(e.target.value)) .value gives string so we used number function to get number and not string from .value
+    if (!description) return; // if acts like guard function if there is no description it returns here itself and prevents submission of null string //
+
+    const newItem = { description, Quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    onAddItems(newItem);
+
+    setdescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handelsubmit}>
+      <h3>what do you need for your trip?</h3>
+      <select
+        value={Quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setdescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
